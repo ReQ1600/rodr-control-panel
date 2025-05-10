b3adc58d35abb90630b5701b2c1956c1c24c9e34
RaQ1600 <drogowskimateusz8@gmail.com>
2025-05-10 19:43:56 +0200

    Cmake configured for winsock, udp and tcp connection

--- rodrcontrolpanel.cpp
+++ rodrcontrolpanel.cpp
@@ -2,18 +2,42 @@
 #include "./ui_rodrcontrolpanel.h"
 
 #include <QValidator>
+#include <QtConcurrent>
 
-namespace Connection
+#include "TCPClient.hpp"
+#include "UDPCommunication.hpp"
+
+namespace rodr
 {
-    enum class Status
+    namespace udp
     {
-        Disconnected = 0,
-        Connected
-    };
+        constexpr u_short REMOTE_PORT = 4000;
+        constexpr u_short LOCAL_PORT = 2000;
+    }
 
-    static bool UDP = static_cast<bool>(Status::Disconnected);
-    static bool TCP = static_cast<bool>(Status::Disconnected);
+    namespace tcp
+    {
+        constexpr u_short PORT = 2000;
+    }
+    namespace connection
+    {
+        constexpr const char* REMOTE_IP = "192.168.113.5";
+        constexpr const char* SOURCE_IP = "192.168.113.4";
+
+        enum class Status
+        {
+            Disconnected = 0,
+            Connected
+        };
+
+        std::unique_ptr<rodr::udp::UDP> feedback_connection;
+        std::unique_ptr<rodr::tcp::TCPClient> tcp_client;
+
+        static Status UDP_status = Status::Disconnected;
+        static Status TCP_status = Status::Disconnected;
+    }
 }
+
 
 
 RODRControlPanel::RODRControlPanel(QWidget *parent)
@@ -37,13 +61,95 @@ RODRControlPanel::~RODRControlPanel()
 
 void RODRControlPanel::on_btnConnectTCP_clicked()
 {
-    ui->lblStatusTCP->setText("connecting...");
+    rodr::connection::tcp_client.reset();
+
+    if (!(bool)rodr::connection::TCP_status)
+    {
+        ui->lblStatusTCP->setText("connecting...");
+        ui->btnConnectTCP->setEnabled(false);
+
+        QCoreApplication::processEvents();
+
+        //runnning concurrently so the app wont go into not responding state
+
+        QtConcurrent::run([this](){
+            try
+            {
+                rodr::connection::tcp_client = std::make_unique<rodr::tcp::TCPClient>(rodr::connection::REMOTE_IP, rodr::tcp::PORT);
+            } catch (std::exception)
+            {
+                rodr::connection::TCP_status = rodr::connection::Status::Disconnected;
+                ui->lblStatusTCP->setText("not connected");
+                ui->btnConnectTCP->setEnabled(true);
+                return;
+            }
+
+            if (rodr::connection::tcp_client->Test().first)
+            {
+                rodr::connection::TCP_status = rodr::connection::Status::Connected;
+                ui->lblStatusTCP->setText("connected");
+                ui->btnConnectTCP->setText("Disconnect");
+            }
+            else
+            {
+                rodr::connection::TCP_status = rodr::connection::Status::Disconnected;
+                ui->lblStatusTCP->setText("not connected");
+            }
+
+            ui->btnConnectTCP->setEnabled(true);
+        });
+    }
+    else
+    {
+        rodr::connection::tcp_client.reset();
+        rodr::connection::TCP_status = rodr::connection::Status::Disconnected;
+
+        ui->lblStatusTCP->setText("not connected");
+        ui->btnConnectTCP->setText("Connect");
+    }
+
 }
 
 
 void RODRControlPanel::on_btnConnectUDP_clicked()
 {
-    ui->lblStatusUDP->setText("connecting...");
+    rodr::connection::feedback_connection.reset();
+
+    if (!(bool)rodr::connection::UDP_status)
+    {
+        ui->lblStatusUDP->setText("connecting...");
+        ui->btnConnectUDP->setEnabled(false);
+
+        QCoreApplication::processEvents();
+
+        //runnning concurrently so the app wont go into not responding state
+        QtConcurrent::run([this](){
+            try
+            {
+                rodr::connection::feedback_connection = std::make_unique<rodr::udp::UDP>(rodr::connection::SOURCE_IP, rodr::connection::REMOTE_IP,
+                                                                                         rodr::udp::LOCAL_PORT, rodr::udp::REMOTE_PORT);
+            } catch (std::exception)
+            {
+                rodr::connection::UDP_status = rodr::connection::Status::Disconnected;
+                ui->lblStatusUDP->setText("not connected");
+                ui->btnConnectUDP->setEnabled(true);
+                return;
+            }
+
+            rodr::connection::UDP_status = rodr::connection::Status::Connected;
+            ui->lblStatusUDP->setText("connected");
+            ui->btnConnectUDP->setText("Disconnect");
+            ui->btnConnectUDP->setEnabled(true);
+        });
+    }
+    else
+    {
+        rodr::connection::feedback_connection.reset();
+        rodr::connection::UDP_status = rodr::connection::Status::Disconnected;
+
+        ui->lblStatusUDP->setText("not connected");
+        ui->btnConnectUDP->setText("Connect");
+    }
 }
 
 
